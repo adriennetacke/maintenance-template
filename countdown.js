9a49b7c55e449d6cbb5149a0929450c3ff95f0b5
Adrienne Tacke <adrienne.tacke@gmail.com>
2018-03-30 15:01:31 -0700

    setup: initial commit

---
+++ countdown.js
@@ -0,0 +1,48 @@
+function countdown(endDate) {
+	let timer, days, hours, minutes, seconds;
+  
+	endDate = new Date(endDate).getTime();
+
+	if (isNaN(endDate)) {
+		return;
+	}
+  
+	timer = setInterval(calculate, 1000);
+
+	function calculate() {
+		let startDate = new Date();
+		
+		startDate = new Date(startDate.getUTCFullYear(),
+							 startDate.getUTCMonth(),
+													startDate.getUTCDate(),
+													startDate.getUTCHours(),
+													startDate.getUTCMinutes(),
+													startDate.getUTCSeconds());
+		
+		let timeRemaining = parseInt((endDate - startDate.getTime()) / 1000);
+
+		if (timeRemaining >= 0) {
+			days    = parseInt(timeRemaining / 86400);
+			timeRemaining   = (timeRemaining % 86400);
+
+			hours   = parseInt(timeRemaining / 3600);
+			timeRemaining   = (timeRemaining % 3600);
+
+			minutes = parseInt(timeRemaining / 60);
+			timeRemaining   = (timeRemaining % 60);
+
+			seconds = parseInt(timeRemaining);
+
+			document.getElementById("days").innerHTML    = parseInt(days, 10);
+			document.getElementById("hours").innerHTML   = ("0" + hours).slice(-2);
+			document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+			document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+		} else {
+			return;
+		}
+	}
+}
+
+(function () { 
+  countdown('04/01/2018 05:00:00 PM'); 
+}());
